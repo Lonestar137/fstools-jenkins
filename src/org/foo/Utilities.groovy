572c2c3a7a3e9882bb888c28d44d3fc2ffa0ecb4
Garrett Jones <garretcj2010@hotmail.com>
2024-04-21 22:25:57 -0500

    test

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,19 +1,18 @@
 package org.foo
 
-class Util {
-  def steps
-  Util(steps) {this.steps = steps}
-  def test() {
-    println "HELLO WORLD"
-  }
-}
+// class Utilities implements Serializable {
+//   def steps
+//   Utilities(steps) {this.steps = steps}
+//   def mvn(args) {
+//     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+//   }
+//   def test() {
+//     println "HELLO WORLD"
+//   }
+// }
 
-class Utilities implements Serializable {
+class Utilities {
   def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
   def test() {
     println "HELLO WORLD"
   }
