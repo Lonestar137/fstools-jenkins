c9b3c06cfc7fcfd0984611807573c650efdcd4f0
Garrett Jones <garretcj2010@hotmail.com>
2024-04-23 21:47:47 -0500

    Add podman functions

---
+++ vars/podman/podman.groovy
@@ -0,0 +1,30 @@
+def withPodmanContainer(String imageName, List<String> arguments, Closure closure) {
+    // Start the Podman container
+    def containerId = startPodmanContainer(imageName, arguments)
+    
+    try {
+        // Execute the closure inside the container
+        closure.call()
+    } finally {
+        // Stop the Podman container
+        stopPodmanContainer(containerId)
+    }
+}
+
+def startPodmanContainer(String imageName, List<String> arguments) {
+    // Implement the logic to start the Podman container here
+    // For example:
+    def command = ['podman', 'run', '-d', imageName] + arguments
+    def process = command.execute()
+    process.waitFor()
+    def containerId = process.text.trim()
+    return containerId
+}
+
+def stopPodmanContainer(String containerId) {
+    // Implement the logic to stop the Podman container here
+    // For example:
+    def command = ['podman', 'stop', containerId]
+    def process = command.execute()
+    process.waitFor()
+}
